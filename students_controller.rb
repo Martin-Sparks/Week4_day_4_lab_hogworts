edc1e747c3cba026b31a56121ffa42c9e5d2b304
Martin-Sparks <martin.sparks82@gmail.com>
2020-04-23 12:00:58 +0100

    added part  edit funtion

--- students_controller.rb
+++ students_controller.rb
@@ -28,15 +28,23 @@ also_reload('./models/*')
 # create
 
     post '/students' do # create
-        @order = Student.new( params )
-        @order.save()
+        @student = Student.new( params )
+        @student.save()
         erb( :create )
     end
 
 
 # edit
 
-    
+get '/students/:id/edit' do # edit
+    @students = Student.find( params[:id] )
+    erb( :edit )
+  end
+
+  post '/students/:id' do # update
+    Student.new( params ).update
+    redirect to '/students'
+  end
 
 # update
 
